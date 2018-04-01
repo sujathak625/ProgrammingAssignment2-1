d38e35d3f312028188983c53cbb6e7f64f926807
Sujatha Kannan <ksujatha625@gmail.com>
2018-04-01 14:20:47 +0200

    Changed a description
    Week 2 Lexical scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,35 +1,3 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeVector <- function(x = numeric()) {
-        m <- NULL
-        set <- function(y) {
-                x <<- y
-                m <<- NULL
-        }
-        get <- function() x
-        setmean <- function(mean) m <<- mean
-        getmean <- function() m
-        list(set = set, get = get,
-             setmean = setmean,
-             getmean = getmean)
-}
-
-## Write a short comment describing this function
-
-cachemean <- function(x, ...) {
-        m <- x$getmean()
-        if(!is.null(m)) {
-                message("Mean cached already")
-                return(m)
-        }
-        data <- x$get()
-        m <- mean(data, ...)
-        x$setmean(m)
-        m
-}
 
 
 
@@ -98,8 +66,44 @@ cacheSolve <- function(x) {
         inverse1 <- solve(mat)
 
         # Sets the inverse in the cache
-        x$setInverse(inverse1 )
+        x$setInverse(inverse1)
 
         # prints the inversed matrix
         inverse1 
+}
+
+
+#### Default function
+
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeVector <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
+}
+
+## Write a short comment describing this function
+
+cachemean <- function(x, ...) {
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("Mean cached already")
+                return(m)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
 }
